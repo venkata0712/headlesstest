0f3bfe1e32b3744b50a089ba413b29d954d87c9a
venkat <venkat.subrahmanyam@shanrohi.com>
2019-07-22 13:04:29 +0530

    test commit

--- headless/src/headless/SampleTest.java
+++ headless/src/headless/SampleTest.java
@@ -6,18 +6,19 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class hTest {
+public class SampleTest {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-            System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+            System.setProperty("webdriver.chrome.driver", "C:\\Softwares\\chromedriver.exe");
             ChromeOptions chromeOptions = new ChromeOptions();
-//            chromeOptions.addArguments("headless");
+            //chromeOptions.addArguments("headless");
+            chromeOptions.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors");  
+            WebDriver driver = new ChromeDriver(chromeOptions);  
+            
 //            chromeOptions.addArguments("--no-sandbox");
 //            chromeOptions.addArguments("'--ignore-certificate-errors'");
 
-//            ChromeDriver driver = new ChromeDriver(chromeOptions);
-            chromeOptions.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors");  
-            WebDriver driver = new ChromeDriver(chromeOptions);  
+          
 
             driver.get("https://google.com");
 
